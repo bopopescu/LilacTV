ca44705820eecdea5d4b125a98e0f34dc199c327
badjin <railrac23@gmail.com>
2019-06-25 14:32:41 +1000

    update Python & Ruby

--- Study/ruby/class.rb
+++ Study/ruby/class.rb
@@ -1,15 +1,20 @@
 class Cal
   attr_reader :v1, :v2
   attr_writer :v1
+  @@_history = []
   def initialize(v1,v2)
     @v1 = v1
     @v2 = v2
   end
   def add()
-    return @v1+@v2
+    result = @v1+@v2
+    @@_history.push("add : #{@v1}+#{@v2}=#{result}")
+    return result
   end
   def subtract()
-    return @v1-@v2
+    result = @v1-@v2
+    @@_history.push("subtract : #{@v1}-#{@v2}=#{result}")
+    return result
   end
   def setV1(v)
     if v.is_a?(Integer)
@@ -19,21 +24,38 @@ class Cal
   def getV1()
     return @v1
   end
+  def Cal.history()
+    for item in @@_history
+      p item
+    end
+  end
+  def info()
+    return "Cal => v1 : #{@v1}, v2 : #{@v2}"
+  end
 end
 class CalMultiply < Cal
   def multiply()
-    return @v1*@v2
+    result = @v1*@v2
+    @@_history.push("multipy : #{@v1}*#{@v2}=#{result}")
+    return result
+  end
+  def info()
+    return "CalMultiply => #{super()}"
   end
 end
 class CalDivide < CalMultiply
   def divide()
-    return @v1/@v2
+    result = @v1/@v2
+    @@_history.push("divide : #{@v1}/#{@v2}=#{result}")
+    return result
+  end
+  def info()
+    return "CalDivide => #{super()}"
   end
 end
-c1 = CalMultiply.new(10,10)
-p c1.add()
-p c1.multiply()
+c0 = Cal.new(30, 60)
+p c0.info()
+c1 = CalMultiply.new(10, 10)
+p c1.info()
 c2 = CalDivide.new(20, 10)
-p c2, c2.add()
-p c2, c2.multiply()
-p c2, c2.divide()
+p c2.info()
