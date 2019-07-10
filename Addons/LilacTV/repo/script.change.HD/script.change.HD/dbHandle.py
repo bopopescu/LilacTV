f7b9dba0842ae1b7f45610f936003b27460c8abc
badjin <railrac23@gmail.com>
2019-07-10 15:37:51 +1000

    DB

--- Addons/LilacTV/repo/script.change.HD/script.change.HD/dbHandle.py
+++ Addons/LilacTV/repo/script.change.HD/script.change.HD/dbHandle.py
@@ -29,33 +29,19 @@ from __future__ import print_function
 import sys, os
 import urllib, re
 import fcntl, socket, struct
-import xbmcgui
+
 sys.path.append('/storage/.kodi/addons/script.module.myconnpy/lib/')
 import mysql.connector
 
 def check_in():
-
-    lan = socket.gethostbyname(socket.gethostname())
     wan = re.search(re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'),urllib.urlopen('http://checkip.dyndns.org').read()).group()
-    dialog = xbmcgui.Dialog()
-    if dialog.ok("Test","WAN ip : %s" %wan, "LAN ip : %s" %lan):
-        return "%s"%wan
-
+    return "%s"%wan
 
 def getHwAddr(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
     str = ':'.join(['%02x' % ord(char) for char in info[18:24]])
     return str
-
-"""
-
-Example using MySQL Connector/Python showing:
-* dropping and creating a table
-* inserting 3 rows using executemany()
-* selecting data and showing it
-
-"""
 
 def main(config):
     #output = []
@@ -67,32 +53,33 @@ def main(config):
     # cursor.execute(stmt_drop)
 
     # stmt_create = """
-    # CREATE TABLE user (
+    # CREATE TABLE users (
     #     id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    #     ip_add VARCHAR(30) DEFAULT '' NOT NULL,
+    #     name VARCHAR(30) DEFAULT '' NOT NULL,
     #     email VARCHAR(50),
     #     phone VARCHAR(20),
-    #     mac_add_eth0 VARCHAR(50),
-    #     mac_add_wlan VARCHAR(50),
+    #     memo TEXT,
     #     PRIMARY KEY (id)
     # )"""
     # cursor.execute(stmt_create)
-
-    # message = "abcfadasfasd"
     #
-    # # Insert 3 records
-    # names = ( ('Geert',message), ('Jan',message), ('Michel',message) )
-    # stmt_insert = "INSERT INTO names (name,message) VALUES (%s,%s)"
-    # cursor.executemany(stmt_insert, names)
-    # db.commit()
+    # stmt_create = """
+    # CREATE TABLE devices (
+    #     id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    #     mac_add_eth0 VARCHAR(50) DEFAULT '' NOT NULL,
+    #     mac_add_wlan VARCHAR(50) DEFAULT '' NOT NULL,
+    #     ip_add VARCHAR(30),
+    #     PRIMARY KEY (id)
+    # )"""
+    # cursor.execute(stmt_create)
 
     ip_add = check_in()
     mac_add_eth0 = getHwAddr('eth0')
     mac_add_wlan = getHwAddr('wlan0')
 
-    user = ((ip_add,mac_add_eth0,mac_add_wlan),)
-    stmt_insert = "INSERT INTO user (ip_add,mac_add_eth0,mac_add_wlan) VALUES (%s,%s,%s)"
-    cursor.executemany(stmt_insert, user)
+    device = ((mac_add_eth0, mac_add_wlan, ip_add),)
+    stmt_insert = "INSERT INTO devices (mac_add_eth0, mac_add_wlan, ip_add) VALUES (%s,%s,%s)"
+    cursor.executemany(stmt_insert, device)
     db.commit()
 
     # # Read the names again and print them
@@ -104,7 +91,6 @@ def main(config):
 
     # Cleaning up, dropping the table again
     # cursor.execute(stmt_drop)
-
 
     cursor.close()
     db.close()
