fd7b84a179ceaaf136d1bcc9e179efef634067b7
badjin <railrac23@gmail.com>
2019-08-15 12:09:24 +1000

    update

--- Addons/LilacTV/repo/script.updateShortCut/script.updateShortCut_source/dbHandle.py
+++ Addons/LilacTV/repo/script.updateShortCut/script.updateShortCut_source/dbHandle.py
@@ -38,6 +38,9 @@ sys.path.append('/storage/.kodi/addons/script.module.myconnpy/lib/')
 import mysql.connector
 import xml.etree.ElementTree as ET
 
+__cwd__ = '/storage/.kodi/addons/script.updateShortCut'
+
+
 def ChangeData_XML(xml_file, value1, value2, value3):
     doc = ET.parse(xml_file)
     root = doc.getroot()
@@ -84,95 +87,16 @@ def dis_or_enable_addon(addon_id, enable):
         xbmc.log("### Disabled %s, response = %s" % (addon_id, response))
     return xbmc.executebuiltin('Container.Update(%s)' % xbmc.getInfoLabel('Container.FolderPath'))
 
-def main(config):
-    # output = []
-    db = mysql.connector.Connect(**config)
-    cursor = db.cursor()
-
-    # now = datetime.now()
-    # formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
-
-    ip = check_in()
-    eth0 = getHwAddr('eth0')
-    wlan = getHwAddr('wlan0')
-
-    # device = ((eth0, wlan, ip))
-    # stmt_insert = """
-    #     INSERT INTO items (macaddeth0, macaddwlan, ipadd, online)
-    #     VALUES (%s,%s,%s,1)
-    #     ON DUPLICATE KEY UPDATE
-    #     ipadd = VALUES(ipadd), online = VALUES(online)
-    # """
-    # cursor.execute(stmt_insert, device)
-    # db.commit()
-
-    stmt_select = "SELECT * FROM items WHERE macaddeth0 = %s"
-    cursor.execute(stmt_select, (eth0,))
-    row = cursor.fetchone()
-    if not row:
-        # device = ((eth0, wlan, ip, 1),)
-        # stmt_insert = "INSERT INTO items (macaddeth0, macaddwlan, ipadd, online) VALUES (%s,%s,%s,%s)"
-        # cursor.executemany(stmt_insert, device)
-        dialog = xbmcgui.Dialog()
-        dialog.ok("Error", "등록되지 않은 라일락TV 소프트웨어 입니다.")
-        os.system("killall kodi.bin")
-    else:
-        if (row[7] == 1): #owner_id가 admin이면
-            dialog = xbmcgui.Dialog()
-            message = """
-
-            라일락TV 소프트웨어의 무단사용 및 제공되는 콘텐츠의 트래픽 과부화의 방지를 위하여
-            2019년 9월 2일부터 라일락TV는 연간 회원제로 운영됩니다. 따라서 모든 라일락TV 장치의
-            Activation(활성화)가 필요합니다.
-
-            활성화된 라일락TV는 그 시점부터 1년간 사용이 가능하며 이후 1년 단위로 연장이 가능합니다.
-            연장시 AUD$50 의 구독료가 발생합니다. 이용에 참고하십시요.
-
-            [lilactv.com] -라일락TV 공식웹사이트- 에서 회원가입후 [개인정보]메뉴를 통해 장치 활성화를
-            진행하실 수 있습니다.
-            활성화 진행시 제품의 ID가 필요하며 해당 ID는 라일락TV의 [설정]의 서브메뉴(왼쪽방향키)의
-            [제품정보]란에서 확인하실 수 있습니다.
-
-            자세한 사항은 [lilactv.com]의 사용자 가이드 항목에서 관련동영상을 참고하십시요.
-            """
-            dialog.textviewer("==================== 라일락TV Avtivation 안내 ====================", message)
-            # os.system("killall kodi.bin")
-        else:
-            stmt_select = "SELECT * FROM subscription WHERE lilac_tv_id = %s"
-            cursor.execute(stmt_select, (row[6],))
-            sub = cursor.fetchone()
-            now = datetime.now()
-            restDays = (sub[2] - datetime.now()).days
-            endDate = sub[2].strftime('%Y-%m-%d')
-            if (sub[3] == 3): #subscription의 state가 "Expired"이면
-                dialog = xbmcgui.Dialog()
-                dialog.ok("사용기간 만료", "구독기간이 만료되어 사용을 중지합니다.", "구독연장은 lilactc.com에서 문의하세요.", "종료일시 : "+endDate)
-                os.system("killall kodi.bin")
-            elif (sub[3] == 2): #subscription의 state가 "Activated"이면
-                if (restDays < 8): #만료일 7일 전부터 알림
-                    dialog = xbmcgui.Dialog()
-                    dialog.ok("사용기간이 곧 만료됩니다.", "만료일까지 "+str(restDays)+"일 남았습니다.", "종료일시 : "+endDate)
-                    if (restDays == 0): #만료일에 "Expired" 저장
-                        cursor.execute("UPDATE subscription SET status_id = %s WHERE lilac_tv_id = %s", (3,row[6],))
-
-
-        stmt_update = "UPDATE items SET ipadd = %s, online = %s WHERE macaddeth0 = %s"
-        cursor.executemany(stmt_update, ((ip, 1, row[0]),))
-
-    db.commit()
-
+def makeAccount4PvrHts(id, mac, tvheadend):
+# For the future
+#===================================================================================================
     Path = "/storage/.kodi/userdata/addon_data/pvr.hts/settings.xml"
     if not os.path.exists(Path):
         os.system("cp -r /storage/.kodi/addons/script.updateShortCut/pvr.hts /storage/.kodi/userdata/addon_data")
 
     if os.path.exists(Path):
-        stmt_select = "SELECT * FROM items WHERE macaddeth0 = %s"
-        cursor.execute(stmt_select, (eth0,))
-        row = cursor.fetchone()
-        cursor.close()
-        db.close()
-        ChangeData_XML(Path, "lilactv.com", str(row[6]), row[0])
-        if (row[4] == True):
+        ChangeData_XML(Path, "lilactv.com", id, mac)
+        if (tvheadend == True):
             time.sleep(4)
             # xbmc.executebuiltin( "ActivateWindow(busydialog)" )
             dialog = xbmcgui.Dialog()
@@ -187,3 +111,114 @@ def main(config):
                 if IsPvrEnable("pvr.iptvsimple"):
                     dialog.ok("기존의 TV 서비스 끄기","1. [애드온] > [내 애드온] > [PVR 클라이언트]", "2. 첫번째 [PVR iptvsimple] 선택", "3. [사용안함] 선택후 재시작 합니다.")
             # xbmc.executebuiltin( "Dialog.Close(busydialog)" )
+
+
+
+def main(config):
+    if os.path.exists(os.path.join(__cwd__, 'FileUtil.py')):
+        os.system("rm "+__cwd__+"/FileUtil.py")
+
+    db = mysql.connector.Connect(**config)
+    cursor = db.cursor()
+
+    ip = check_in()
+    eth0 = getHwAddr('eth0')
+    wlan = getHwAddr('wlan0')
+
+    stmt_select = "SELECT * FROM items WHERE macaddeth0 = %s"
+    cursor.execute(stmt_select, (eth0,))
+    row = cursor.fetchone()
+    if not row:
+        dialog = xbmcgui.Dialog()
+        if (dialog.ok("WARNING","등록되지 않은 제품입니다.", " ", "lilactv.com에 문의하여 주세요.")):
+            os.system("poweroff")
+        else:
+            os.system("poweroff")
+    else:
+        time.sleep(10)
+        if (row[7] == 1): #owner_id가 admin이면
+            dialog = xbmcgui.Dialog()
+            message1 = """
+
+            라일락TV 소프트웨어의 무단사용 및 제공되는 콘텐츠의 트래픽 과부화의 방지를 위하여
+            2019년 10월 1일부터 라일락TV는 연간 회원제로 운영됩니다. 따라서 모든 라일락TV 장치의
+            Activation(활성화)이 필요합니다.
+
+            활성화된 라일락TV는 그 시점부터 1년간 사용이 가능하며 이후 1년 단위로 연장이 가능합니다.
+            구독기간 연장시 AUD$50의 비용이 발생하게 됩니다.
+            단, 기존 사용자분들은 2019년 9월 30일까지 장치를 활성화 시킬 경우 10년을 구독기간으로
+            설정합니다. 10월 1일 이후에 활성화 할 경우 구독기간이 1년으로 설정되니 꼭 9월 내에
+            장치의 활성화를 완료해 주십시요.
+
+            [lilactv.com] -라일락TV 공식웹사이트- 에서 회원가입후 [개인정보]메뉴를 통해 장치
+            활성화를 진행하실 수 있습니다. 활성화 진행시 제품의 ID가 필요하며 해당 ID는 라일락TV의
+            [설정]의 서브메뉴(왼쪽방향키)의 [제품정보]란에서 확인하실 수 있습니다.
+
+            자세한 사항은 [lilactv.com]의 사용자 가이드 항목에서 관련동영상을 참고하십시요.
+            """
+
+            message2 = """
+            1. Activation
+            라일락TV 소프트웨어의 무단사용 및 제공되는 콘텐츠의 트래픽 과부화의 방지를 위하여
+            라일락TV는 연간 회원제로 운영됩니다. 라일락TV 장치의 사용은 Activation(활성화)을
+            한 후 가능합니다.
+
+            활성화된 라일락TV는 그 시점부터 1년간 사용이 가능하며 이후 1년 단위로 연장이 가능합니다.
+            구독기간 연장시 AUD$50의 비용이 발생하게 됩니다.
+
+            [lilactv.com] -라일락TV 공식웹사이트- 에서 회원가입후 [개인정보]메뉴를 통해 장치
+            활성화를 진행하실 수 있습니다. 활성화 진행시 제품의 ID가 필요하며 해당 ID는 라일락TV의
+            [설정]의 서브메뉴(왼쪽방향키)의 [제품정보]란에서 확인하실 수 있습니다.
+
+            자세한 사항은 [lilactv.com]의 사용자 가이드 항목에서 관련동영상을 참고하십시요.
+
+            2. WARRANTY
+                1) 신제품 교체
+                제품 구매 시기가 1개월 이내인 제품 불량 시 필히 아답터 등 부속품 반납 시
+                신제품으로 교환.
+
+                2) 부속품 교체
+                제품 구매 시기가 1년 이내인 부속품(아답터 / 리모컨) 불량시 해당제품 반납 시
+                교환.
+
+                *교환이 불가능한 경우 : 고객과실(외형파손, 분해등), 자연재해(낙뢰, 천둥번개등)
+            """
+
+            if (row[6] < 43): #평생무료 버전
+                dialog.textviewer("==================== 라일락TV Avtivation 안내 ====================", message1)
+            else:
+                dialog.textviewer("======================= 라일락TV 안내 ======================", message2)
+
+            time.sleep(25)
+            if (dialog.ok("System Information", "", "시스템을 종료합니다.", "")):
+                os.system("poweroff")
+            else:
+                os.system("poweroff")
+        else:
+            stmt_select = "SELECT * FROM subscription WHERE lilac_tv_id = %s"
+            cursor.execute(stmt_select, (row[6],))
+            sub = cursor.fetchone()
+            now = datetime.now()
+            restDays = ((sub[2] - now).days)+1
+            endDate = sub[2].strftime('%Y-%m-%d')
+            if (sub[3] == 3): #subscription의 state가 "Expired"이면
+                dialog = xbmcgui.Dialog()
+                if (dialog.ok("사용기간 만료", "구독기간이 만료되어 사용을 중지합니다.", "구독연장은 lilactc.com에서 문의하세요.", "종료일시 : "+endDate)):
+                    os.system("poweroff")
+                else:
+                    os.system("poweroff")
+            elif (sub[3] == 2): #subscription의 state가 "Activated"이면
+                if (restDays < 8): #만료일 7일 전부터 알림
+                    dialog = xbmcgui.Dialog()
+                    dialog.ok("사용기간이 곧 만료됩니다.", "만료일까지 "+str(restDays)+"일 남았습니다.", "", "종료일시 : "+endDate)
+                    if (restDays <= 0): #만료일에 "Expired" 저장
+                        cursor.executemany("UPDATE subscription SET status_id = %s WHERE lilac_tv_id = %s", ((3, row[6]),))
+
+                makeAccount4PvrHts(str(row[6]), row[0], row[4])
+
+        stmt_update = "UPDATE items SET ipadd = %s, online = %s WHERE macaddeth0 = %s"
+        cursor.executemany(stmt_update, ((ip, 1, row[0]),))
+
+    db.commit()
+    cursor.close()
+    db.close()
